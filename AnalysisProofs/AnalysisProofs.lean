81dc79b3b453bfe63f472a1d60ac8305f37b1ab8
Austin Anderson <austinanderson@Austins-MacBook-Air.local>
2025-05-29 16:11:28 -1000

    trying

--- AnalysisProofs/AnalysisProofs.lean
+++ AnalysisProofs/AnalysisProofs.lean
@@ -1,7 +1,29 @@
--- This module serves as the root of the `AnalysisProofs` library.
--- Import modules here that should be built as part of the library.
-import AnalysisProofs.Basic
+import Mathlib.Data.Nat.Basic
+import Mathlib.Tactic.Ring.Basic
+import Mathlib.Data.Real.Basic
 
-theorem 0PlusNisN (n : ℕ) : 0 + n = n := by {
 
+theorem ZeroPlusNisN (n : ℕ) : 0 + n = n := by {
+  induction' n with d hd
+  rfl
+  rw [← hd]
+  rw [Nat.add_assoc]
+  simp
+}
+
+#print ZeroPlusNisN
+
+def LowerBoundReal (r : ℝ) (A : Set ℝ) : Prop :=
+  (∀ a ∈ A, a ≥ r)
+
+def GreatestLowerBoundReal (r: ℝ) (A : Set ℝ) : Prop :=
+  (LowerBoundReal r A) ∧ (∀ s : ℝ, (LowerBoundReal s A) → r ≥ s)
+
+def reciprocalsOfNaturalNumbers : Set ℝ :=
+  { r : ℝ | ∃ n : ℕ, n ≠ 0 ∧ r = 1 / n }
+
+lemma Lemma1 : GreatestLowerBoundReal 0 reciprocalsOfNaturalNumbers := by {
+  unfold GreatestLowerBoundReal
+  constructor
+  sorry
 }
