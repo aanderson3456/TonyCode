dc967cdd8643a0f453904615f6b8c25056ab6c45
Austin Anderson <austinanderson@Austins-MacBook-Air.local>
2025-05-29 15:51:20 -1000

    started proof file

---
+++ AnalysisProofs/AnalysisProofs.lean
@@ -0,0 +1,7 @@
+-- This module serves as the root of the `AnalysisProofs` library.
+-- Import modules here that should be built as part of the library.
+import AnalysisProofs.Basic
+
+theorem 0PlusNisN (n : ℕ) : 0 + n = n := by {
+
+}
